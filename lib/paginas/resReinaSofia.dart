9d3caca21283802cf27171d082d8e7de0b92b88b
SStefanV <rakulle@gmail.com>
2021-01-31 01:11:05 +0100

    HelperClass

--- lib/paginas/resReinaSofia.dart
+++ lib/paginas/resReinaSofia.dart
@@ -12,7 +12,7 @@ class ResReinaSofia extends StatelessWidget {
     'Avda. Club Deportivo, 33(Edificio Arsys)\n\n7:20 14:20 21:20',
   ];
   String name = 'Residencia R.Sofia';
-  String cod = '40';
+  String cod = '312';
   Helper help = Helper();
   @override
   Widget build(BuildContext context) {
